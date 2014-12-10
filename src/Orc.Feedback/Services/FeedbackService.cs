a77cd7abf58336f66926c771f45e3a4fadf33893
Geert van Horrik <geert@catenalogic.com>
2014-12-10 11:09:00 +0100

    Added very basic implementation (open url in browser for now)

---
+++ src/Orc.Feedback/Services/FeedbackService.cs
@@ -0,0 +1,35 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="FeedbackService.cs" company="Orchestra development team">
+//   Copyright (c) 2008 - 2014 Orchestra development team. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+
+namespace Orc.Feedback
+{
+    using System.Threading.Tasks;
+    using Catel;
+    using Catel.Services;
+
+    public class FeedbackService : IFeedbackService
+    {
+        private readonly IProcessService _processService;
+
+        public FeedbackService(IProcessService processService)
+        {
+            Argument.IsNotNull(() => processService);
+
+            _processService = processService;
+        }
+
+        public string ApiToken { get; set; }
+
+        public string Url { get; set; }
+
+        public async Task ProvideFeedback()
+        {
+            // for now, just open the url in the browser
+            _processService.StartProcess(Url);
+        }
+    }
+}
---
+++ src/Orc.Feedback/Services/Interfaces/IFeedbackService.cs
@@ -0,0 +1,17 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="IFeedbackService.cs" company="Orchestra development team">
+//   Copyright (c) 2008 - 2014 Orchestra development team. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+
+namespace Orc.Feedback
+{
+    using System.Threading.Tasks;
+
+    public interface IFeedbackService
+    {
+        string Url { get; set; }
+        Task ProvideFeedback();
+    }
+}

75b498194c03e1decb0aa9716971a778b78e2d03
Geert van Horrik <geert@catenalogic.com>
2017-10-01 19:18:54 +0200

    Enable ApiApprover to make sure we don't introduce unwanted breaking changes

---
+++ src/Orc.Feedback.Tests/Orc.Feedback.Tests.Shared/Orc.Feedback.NET47.approved.cs
@@ -0,0 +1,25 @@
+﻿[assembly: System.Resources.NeutralResourcesLanguageAttribute("en-US")]
+[assembly: System.Runtime.InteropServices.ComVisibleAttribute(false)]
+[assembly: System.Runtime.Versioning.TargetFrameworkAttribute(".NETFramework,Version=v4.7", FrameworkDisplayName=".NET Framework 4.7")]
+
+
+public class static ModuleInitializer
+{
+    public static void Initialize() { }
+}
+namespace Orc.Feedback
+{
+    
+    public class FeedbackService : Orc.Feedback.IFeedbackService
+    {
+        public FeedbackService(Catel.Services.IProcessService processService) { }
+        public string ApiToken { get; set; }
+        public string Url { get; set; }
+        public System.Threading.Tasks.Task ProvideFeedbackAsync() { }
+    }
+    public interface IFeedbackService
+    {
+        string Url { get; set; }
+        System.Threading.Tasks.Task ProvideFeedbackAsync();
+    }
+}
---
+++ src/Orc.Feedback.Tests/Orc.Feedback.Tests.Shared/PublicApiFacts.cs
@@ -0,0 +1,24 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="PublicApiFacts.cs" company="WildGums">
+//   Copyright (c) 2008 - 2017 WildGums. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+
+namespace Orc.Feedback.Tests
+{
+    using ApiApprover;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class PublicApiFacts
+    {
+        [Test]
+        public void Orc_Feedback_HasNoBreakingChanges()
+        {
+            var assembly = typeof(FeedbackService).Assembly;
+
+            PublicApiApprover.ApprovePublicApi(assembly);
+        }
+    }
+}

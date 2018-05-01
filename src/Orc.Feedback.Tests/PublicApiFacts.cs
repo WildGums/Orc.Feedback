7560549d44b280b43c6d28b1995ece07350e9113
Geert van Horrik <geert@catenalogic.com>
2018-05-01 10:12:22 +0200

    Migrate test project to new project structure

--- src/Orc.Feedback.Tests/PublicApiFacts.cs
+++ src/Orc.Feedback.Tests/PublicApiFacts.cs
@@ -7,13 +7,14 @@
 
 namespace Orc.Feedback.Tests
 {
+    using System.Runtime.CompilerServices;
     using ApiApprover;
     using NUnit.Framework;
 
     [TestFixture]
     public class PublicApiFacts
     {
-        [Test]
+        [Test, MethodImpl(MethodImplOptions.NoInlining)]
         public void Orc_Feedback_HasNoBreakingChanges()
         {
             var assembly = typeof(FeedbackService).Assembly;

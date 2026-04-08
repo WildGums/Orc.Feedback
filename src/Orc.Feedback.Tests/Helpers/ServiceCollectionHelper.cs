eb01d3276a5748daed818d1eaf1d8d5a3e4c8d3b
Geert van Horrik <geert@catenalogic.com>
2026-04-08 09:26:12 +0200

    Convert to file scoped namespaces

--- src/Orc.Feedback.Tests/Helpers/ServiceCollectionHelper.cs
+++ src/Orc.Feedback.Tests/Helpers/ServiceCollectionHelper.cs
@@ -1,19 +1,18 @@
-﻿namespace Orc.Feedback.Tests
+﻿namespace Orc.Feedback.Tests;
+
+using Catel;
+using Microsoft.Extensions.DependencyInjection;
+
+internal static class ServiceCollectionHelper
 {
-    using Catel;
-    using Microsoft.Extensions.DependencyInjection;
-
-    internal static class ServiceCollectionHelper
+    public static IServiceCollection CreateServiceCollection()
     {
-        public static IServiceCollection CreateServiceCollection()
-        {
-            var serviceCollection = new ServiceCollection();
+        var serviceCollection = new ServiceCollection();
 
-            serviceCollection.AddLogging();
-            serviceCollection.AddCatelCore();
-            serviceCollection.AddOrcFeedback();
+        serviceCollection.AddLogging();
+        serviceCollection.AddCatelCore();
+        serviceCollection.AddOrcFeedback();
 
-            return serviceCollection;
-        }
+        return serviceCollection;
     }
 }
